e3c4264ef8a2b46510503c73218c3a73bdcda700
varunkumarnr <varunnrk@gmail.com>
2025-08-09 13:36:23 +0530

    final code

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,6 @@ class Config:
     """Base configuration."""
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:admin@localhost:5432/AgentORC')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PS_CHAT_TOKEN = os.getenv('PS_CHAT_TOKEN')
     GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
     TAVILY_API_KEY=os.getenv('TAVILY_API_KEY')
     JIRA_API_TOKEN=os.getenv('JIRA_API_TOKEN')
